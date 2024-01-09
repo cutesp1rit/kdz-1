ad517f40d1af99183a2c190c4d6fa90de2147585
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-01-09 12:09:28 +0300

    Сделать первый коммит и залить на github

---
+++ firstTask/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace firstTask
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Заполнение N, пока оно не будет заполнено корректно
+        /// </summary>
+        /// <param name="N">Количество строк массива</param>
+        public static void CorrectDataForN(out int N)
+        {
+            Console.WriteLine("Введите N, оно должно быть натуральным и не больше 13: ");
+            while (!(int.TryParse(Console.ReadLine(), out N) && (N > 0) && (N < 14)))
+            {
+                Console.WriteLine("Введены неверные данные, перечитайте условие на них и повторите ввод.");
+            }
+        }
+
+        /// <summary>
+        /// Заполнение M, пока оно не будет заполнено корректно
+        /// </summary>
+        /// <param name="M">Количество столбцов массива</param>
+        public static void CorrectDataForM(out int M)
+        {
+            Console.WriteLine("Введите M, оно должно быть натуральным и не больше 17: ");
+            while (!(int.TryParse(Console.ReadLine(), out M) && (M > 0) && (M < 18)))
+            {
+                Console.WriteLine("Введены неверные данные, перечитайте условие на них и повторите ввод.");
+            }
+        }
+
+        /// <summary>
+        /// Метод заполнения массива через функцию
+        /// </summary>
+        /// <param name="B">Массив для записи значений функции</param>
+        /// <param name="N">Количество строк</param>
+        /// <param name="M">Количество столбцов</param>
+        public static void Filling(out double[][] B, int N, int M)
+        {
+            B = new double[N][];
+            double n = 0;
+
+            for (int j=0; j<N; j++)
+            {
+                B[j] = new double[M];
+                for (int i = 0; i < M; i++)
+                {
+                    B[j][i] = Math.Pow((6.0 * n * n + 3 * n + 2) / (6 * n * n + 3 * n + 3), n); // присваивание элементу массива значение функции
+                    n++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание файла
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="M">Количество столбцов</param>
+        /// <param name="N">Количество строк</param>
+        /// <param name="A">Массив с данными</param>
+        public static void CreatingFile(string directory, int M, int N, double[][] A)
+        {
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name.Length == 0) // Если пользователь не ввел имя, то запускаем цикл заново.
+                {
+                    Console.WriteLine("Вы не ввели название файла, пожалуйста, повторите ввод:");
+                    continue;
+                }
+                string path = directory + Path.DirectorySeparatorChar + name + ".txt"; // создание пути для нового файла или поиска старого
+
+                if (File.Exists(path)) // если уже существует такой файл, то очищаем его
+                {
+                    Console.WriteLine("Такой файл уже существует, поэтому данные будут перезаписаны.");
+                    // очищение уже существующего файла от старых данных
+                    FileStream fileStream = File.Open(path, FileMode.Open);
+                    fileStream.SetLength(0);
+                    fileStream.Close();
+                }
+
+                try
+                {
+                    // создание файла и заполнение его данными
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine($"{N}");
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < M; j++)
+                            {
+                                sw.Write($"{A[i][j]:f3}  ");
+                            }
+                            sw.WriteLine();
+                        }
+                    }
+                    Console.WriteLine("Данные записаны успешно!");
+                    break;
+                }
+                catch (IOException ex) // поимка одного из исключений
+                {
+                    Console.WriteLine("Введено некорректное название файла. Повторите попытку:");
+                    continue;
+                }
+                catch (Exception ex) // поимка остальных
+                {
+                    Console.WriteLine("Возникла непредвиденная ошибка, повторите попытку:");
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Основной метод с вызывами всех функций и бесконечным циклом.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                // ввод данных и проверка их на корректность
+                int N, M;
+                CorrectDataForN(out N);
+                CorrectDataForM(out M);
+
+                double[][] A; // создание массива
+                Filling(out A, N, M); // заполнение массива с помощью метода
+                string directory = System.IO.Directory.GetParent(System.IO.Directory.GetParent(System.IO.Directory.GetParent(System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString()).ToString(); // поиск директории с папкой решения
+                Console.WriteLine("Для создания файла задайте ему название: ");
+                CreatingFile(directory, M, N, A);
+                Console.WriteLine("Если вы хотите выйти из программы, то нажмите ESC, в ином случае программа повторится...");
+            } while(Console.ReadKey().Key!= ConsoleKey.Escape);
+        }
+    }
+}
---
+++ secondTask/Program.cs
@@ -0,0 +1,159 @@
+﻿using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace secondTask
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Отвечает за наполнение нового видоизмененного массива с помощью полученного массива из файла (B)
+        /// </summary>
+        /// <param name="B">Старый массив</param>
+        /// <param name="C">Новый массив</param>
+        public static void Slice(ref double[][] B, ref double[][] C)
+        {
+            int k = 0, m = 0;
+            for (int i=0; i<B.Length; i++)
+            {
+                C[i] = new double[B[i].Length - 1];
+                m = 0;
+                for (int j = 0; j < B[i].Length; j++)
+                {
+                    if (i == j) // условие для сдвига
+                    {
+                        continue;
+                    }
+                    C[k][m] = B[i][j];
+                    m++;
+                }
+                k++;
+            }
+        }
+
+        /// <summary>
+        /// Чтение файла с ловлей ошибок связанных с содержимом файла. Если появляется такая ошибка, то возвращает false, за счет чего в Main вызываем заново.
+        /// Возвращаем его перезапись в массив B. Вызов метода для формирования нового массива по созданию и вызов функции по выводу массивов на консоль.
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        public static bool Reading(string directory)
+        {
+
+            string path = directory + Path.DirectorySeparatorChar + Console.ReadLine() + ".txt"; // построение пути к файлу
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line = sr.ReadLine(); // считывание первой строки
+                int N; // первое число файла (размерность массива)
+                int i = 0, j = 0; // индексы для занесения данных в массив
+                if (line == null || line.Length == 0) // ловля одной из ошибок
+                {
+                    Console.WriteLine("Файл пустой, повторите попытку:");
+                    return false;
+                }
+                //проверка данных в файле на корректность 
+                if (!int.TryParse(line, out N))
+                {
+                    Console.WriteLine("Нарушен формат данных в файле. Пожалуйста, выберите другой:");
+                    return false;
+                }
+                double[][] B = new double[N][]; // создание массива для переноса данных из файла
+                line = sr.ReadLine();
+                while (line != null) // цикл для считывания всех строк
+                {
+                    string[] elements = line.Split("  ", StringSplitOptions.RemoveEmptyEntries); // разделение строки по элементам
+                    B[i] = new double[elements.Length];
+                    foreach (string element in elements) // цикл добавления элементов в массив
+                    {
+                        double massiv_element;
+                        if (!double.TryParse(element, out massiv_element)) // проверка данных на корректность
+                        {
+                            Console.WriteLine("Нарушен формат данных в файле. Пожалуйста, выберите другой:");
+                            return false;
+                        }
+                        B[i][j] = massiv_element;
+                        j++;
+                    }
+                    line = sr.ReadLine();
+                    i++;
+                    j = 0;
+                    N--; // уменьшая N, мы потом сможем проверить сопадает ли оно с истинным размером массива (должно стать равно 0 в таком случае)
+                }
+                if (N!=0)
+                {
+                    Console.WriteLine("Истинный размер массива и первое значение файла не совпадают. Пожалуйста, выберите другой:");
+                    return false;
+                }
+                double[][] C = new double[B.Length][];
+                Slice(ref B, ref C);
+                Print(B, C);  
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Вывод двух полученных массивов
+        /// </summary>
+        /// <param name="B">Старый массив</param>
+        /// <param name="C">Новый массив</param>
+        public static void Print(double[][] B, double[][] C)
+        {
+            for (int i = 0; i < B.Length; i++)
+            {
+                for (int j = 0; j < B[i].Length; j++)
+                {
+                    Console.Write($"{B[i][j]:f3} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < C.Length; i++)
+            {
+                for (int j = 0; j < C[i].Length; j++)
+                {
+                    Console.Write($"{C[i][j]:f3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Основной метод с вызывами всех функций и проверки данных с помощью try catch.
+        /// </summary>
+        static void Main()
+        {
+            do {
+                Console.Clear();
+                Console.WriteLine("Введите название файла, который хотите открыть:");
+                string directory = System.IO.Directory.GetParent(System.IO.Directory.GetParent(System.IO.Directory.GetParent(System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString()).ToString(); // поиск дериктории с файлами из прошлой задачи
+                while (true)
+                {
+                    try // ловля исключений
+                    {
+                        if (Reading(directory)==false)
+                        {
+                            continue;
+                        }
+                        break;
+                    }
+                    catch (FileNotFoundException ex) // исключение отсутствие файла
+                    {
+                        Console.WriteLine("Такой файл не существует, повторите попытку:");
+                        continue;
+                    }
+                    catch (IOException ex) // поимка исключения при вводе некорректных символов
+                    {
+                        Console.WriteLine("Введено некорректное название файла. Повторите попытку:");
+                        continue;
+                    }
+                    catch (Exception ex) // поимка остальных
+                    {
+                        Console.WriteLine("Возникла непредвиденная ошибка, повторите попытку:");
+                        continue;
+                    }
+                }
+                Console.WriteLine("Если вы хотите выйти из программы, то нажмите ESC, в ином случае программа повторится...");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape); // продолжение программы по желанию пользователя
+        }
+    }
+}
